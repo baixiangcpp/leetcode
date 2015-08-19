321d05a8b46255cfabfdefa9cd247c2be6acb744
baixiangcpp <baixiangcpp@gmail.com>
2015-08-19 21:08:09 +0800

    use 'set',easy

--- src/easy/Contains Duplicate.cpp
+++ src/easy/Contains Duplicate.cpp
@@ -3,7 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         set<int> m;
 		pair<set<int>::iterator,bool> p;
-		for(auto it = nums.begin();it!=nums.end();++it)
+		
+        for(auto it = nums.begin();it!=nums.end();++it)
 		{
 			p = m.insert(*it);
 			if(!p.second)
