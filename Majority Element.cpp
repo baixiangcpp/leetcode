4358d2d04918a0c97daba9dc38785ffc3fb5e27e
baixiangcpp <baixiangcpp@gmail.com>
2015-08-19 20:14:16 +0800

    111111
    1111111

--- Majority Element.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int count = nums.size()/2;
-		sort(nums.begin(),nums.end());
-		auto it = nums.begin() + count;
-		return *it;
-    }
-};
