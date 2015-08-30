d303a8b84e435b593ac4e69b88aef7208a67188f
baixiangcpp <baixiangcpp@gmail.com>
2015-08-30 20:54:02 +0800

    dynamic programming

--- src/easy/House Robber.cpp
+++ src/easy/House Robber.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    int rob(vector<int>& nums) {
+        if(!nums.size())
+            return 0;
+        vector<int> dp(nums);
+        for(int i=0;i<nums.size();++i)
+        {
+            if(i == 0)
+                dp[i] = nums[i];
+            else if(i == 1)
+                dp[i] = max(nums[0],nums[1]);
+            else
+                dp[i] = max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp.back();
+    }
+};
+/*class Solution {
+public:
     int rob(vector<int>& nums) {//dynamic programming
         int left = 0;
         int right =0;
@@ -11,4 +29,4 @@ public:
         }
         return max(left,right);
     }
-};
+};*/
